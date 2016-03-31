3d0635f67a589c78a6a5903e639f5a1ce314903f
sheettrait <sheettrait@hotmail.com>
2016-03-31 17:19:40 +0800

    all command
    HI

--- Main.py
+++ Main.py
@@ -7,12 +7,15 @@ import sys
 import urllib.request
 import tkinter
 import datetime
+import threading
+
 from bs4 import BeautifulSoup
 from yahoo_finance import Share
 import urllib
 from pip._vendor.distlib.compat import raw_input
 from tkinter import Label, Entry
-from tkinter.constants import INSERT, DISABLED
+from tkinter.constants import INSERT
+
 class GetData():
     def __init__(self):
         self.Responese = urllib.request.urlopen('http://www.twse.com.tw/ch/trading/exchange/BWIBBU/BWIBBU_d.php')
@@ -22,6 +25,7 @@ class GetData():
         self.AllData=[]
         self.TrendThreeMen=[]
         self.HistoryData=[]
+#####################
     def SearchPERatio(self):
         for x in range(8,203,5):
        # for x in range(8,self.soup.select('.basic2').__len__(),5):
@@ -62,9 +66,10 @@ class GetData():
     def ThreeMenDollar(self): #三大法人
         Today = datetime.datetime.now().strftime("%Y%m%d")
         url = "http://www.twse.com.tw/ch/trading/fund/BFI82U/BFI82U_print.php?begin_date="+Today+"&report_type=day&language=ch&save=csv" 
+        self.ThreeMenResponse = urllib.request.urlopen(url)
         CSVReader = csv.reader(io.TextIOWrapper(urllib.request.urlopen(url)))
-        for row in CSVReader:
-            print(row)
+      #  for row in CSVReader:
+      #      print(row)
     
     def FeatureToday(self): #期貨
         
@@ -80,12 +85,10 @@ class GetData():
         FeatureRequest= requests.post('http://www.taifex.com.tw/chinese/3/7_12_6dl.asp',data=TodayFeature,allow_redirects=False)
         URLRedirect = "http://www.taifex.com.tw"+FeatureRequest.headers['Location']
         RealCSVFile = requests.get(URLRedirect)
-        response = urllib.request.urlopen(RealCSVFile.url)
-        for row in csv.reader(io.TextIOWrapper(response)):
-    #for x in range(row.__len__()):
-            print(row)
-            
-            
+        self.response = urllib.request.urlopen(RealCSVFile.url)
+      #  for row in csv.reader(io.TextIOWrapper(self.response)):
+       #     print(row)
+
 class GUI(GetData):
     
     def __init__(self):
@@ -93,31 +96,76 @@ class GUI(GetData):
 ####################### initial interface #######################
         self.interface = tkinter.Tk()
         self.interface.title("HI")
- #       self.newLabel=Label()
-   #     self.guitext = tkinter.Text(self.interface,height=2,width=3)
         self.inputPERatio = Label()
         self.inputPERatio["text"]="Input Ratio"
         self.inputPERatio.grid(row=0,column=0)
         self.inputPREField = Entry(self.interface)
         self.inputPREField.bind('<Return>',self.GetTextFromPREField)
         self.inputPREField.grid(row=0,column=1)
+        
         self.inputEPS = Label()
         self.inputEPS["text"]="Input EPS"
         self.inputEPS.grid(row=1,column=0)
         self.inputEPSField = Entry(self.interface)
         self.inputEPSField.bind('<Return>',self.GetTextFromEPSField)
-        self.inputEPSField.grid(row=1,column=1) 
+        self.inputEPSField.grid(row=1,column=1)
+        
+        self.inputVolume = Label()
+        self.inputVolume["text"]="Input Volume"
+        self.inputVolume.grid(row=2,column=0)
+        self.inputVolumeField = Entry(self.interface)
+        self.inputVolumeField.bind('<Return>',self.GetTextFromEPSField)
+        self.inputVolumeField.grid(row=2,column=1)
+        
+        self.inputWannaPrice = Label()
+        self.inputWannaPrice["text"]="Input Close Price"
+        self.inputWannaPrice.grid(row=3,column=0)
+        self.inputWannaPriceField = Entry(self.interface)
+        self.inputWannaPriceField.bind('<Return>',self.GetTextFromEPSField)
+        self.inputWannaPriceField.grid(row=3,column=1)
+         
         self.CheckButton = tkinter.Button(text="Enter")
         self.CheckButton.bind('<Button-1>',self.OutputData)
-        self.CheckButton.grid(row=2)
+        self.CheckButton.grid(row=4)
+        
+        self.Feature = Label()
+        self.Feature["text"] = "三大法人期貨"
+        self.Feature.grid(row=0,column=4)
+        self.FeatureField = tkinter.Text(height=10,width=80)
+        self.FeatureField.grid(row=1,column=4)
+        
         self.Threemen = Label()
         self.Threemen["text"] = "三大法人"
-        self.Threemen.grid(row=0,column=4)
-        self.ThreemenField = tkinter.Text()
-        self.ThreemenField.grid(row=1,column=4)
+        self.Threemen.grid(row=3,column=4)
+        self.ThreemenField = tkinter.Text(height=10,width=80)
+        self.ThreemenField.grid(row=6,column=4)
+        
+        self.FeatureToday()
+        self.PrintFeautre_thread = threading.Thread(target=self.PrintFeautre)
+        self.PrintFeautre_thread.start()
+        
+        self.ThreeMenDollar()
+        self.PrinThreeMenDollar_thread = threading.Thread(target=self.PrintThreeMen())
+        self.PrinThreeMenDollar_thread.start()
 ####################### initial interface #######################        
-    #    self.guitext.grid()
         self.interface.mainloop()
+        
+        
+    
+    def PrintFeautre(self):
+        for row in csv.reader(io.TextIOWrapper(self.response)):
+            print(row)
+            self.FeatureField.insert(INSERT, row[1:4])    
+            
+    def PrintThreeMen(self):
+        for row in csv.reader(io.TextIOWrapper(self.ThreeMenResponse)):
+            self.ThreemenField.insert(INSERT,row[1:2])
+        
+    def GetTextFromVolumeField(self,event):
+        self.TargetPERatio = float(self.inputtVolumeField.get())
+        
+    def GetTextFromWannaPriceField(self,event):
+        self.TargetPERatio = float(self.inputWannaPriceField.get())
         
     def GetTextFromPREField(self,event):
         self.TargetPERatio = float(self.inputPREField.get())
@@ -125,6 +173,7 @@ class GUI(GetData):
     def GetTextFromEPSField(self,event):
         self.TargetEPS=float(self.inputEPSField.get())
         self.SearchPERatio()
+        
     def OutputData(self,event):
         for x in range(self.AllData.__len__()):     #Output the outcome 
             print(self.AllData[x])
@@ -133,14 +182,16 @@ class GUI(GetData):
             for y in range(0,4,1):
                 #tkinter.Text(self.interface,text='hello').grid(row=3+x,column=y)
                # self.guitext.insert(INSERT,"hello...").grid(row=3+x,column=y)
-               tkinter.Label(self.interface,text=self.AllData[4*x+y]).grid(row=3+x,column=y)
-    #    for r in range(self.AllData.__len__()%4):
-    #        for c in range(4):
-    #            tkinter.Text(self.interface,height=1,width=1,borderwidth=1).grid(row=3+r,column=c)
+               
+               self.temp = tkinter.Text(self.interface,height=3,width=3)
+               self.temp.insert(INSERT,self.AllData[x*4+y])
+               self.temp.grid(row=5+x,column=y)
+
+
 
 b = GUI()
-b.FeatureToday()
-b.ThreeMenDollar()
+#b.FeatureToday()
+#b.ThreeMenDollar()
 #b.DayMoving("2891")
 #print(b.HistoryData)
 #b.getihi()
--- beauti.py
+++ beauti.py
@@ -9,10 +9,10 @@ from bs4 import BeautifulSoup
 import urllib
 import chardet
 from time import sleep
-url="http://www.twse.com.tw/ch/trading/fund/BFI82U/BFI82U_print.php?begin_date=20160330&end_date=20160329&report_type=day&language=ch&save=csv"
-#url="http://finance.yahoo.com/d/quotes.csv?s=^TWII&f=l1"
+#url="http://www.twse.com.tw/ch/trading/fund/BFI82U/BFI82U_print.php?begin_date=20160330&end_date=20160329&report_type=day&language=ch&save=csv"
+url="http://finance.yahoo.com/d/quotes.csv?s=^TWII&f=l1"
 while(True):
     CSVReader = csv.reader(io.TextIOWrapper(urllib.request.urlopen(url)))
     for row in CSVReader:
          print(row)
-        # sleep(2)
+         sleep(2)
