0234b2ad83d1910f625e1dee03a08ff8353c2405
Bryant.yang <sheettrait@hotmail.com>
2016-06-22 18:13:44 +0800

    hi

--- DetailAnalysis.py
+++ DetailAnalysis.py
@@ -9,16 +9,14 @@ wb = Workbook()
 ws = wb.active
 reader = csv.reader(open('Daily_2016_06_21.csv'))
 
-a = 1
 row_counter = 1
 
 for row in reader:
     try:
-        if "TX" in row[1] and "MTX" not in row[1]:
-        #    print(row[3][0:2]) 
-        #    print(row)
-        #    print(datetime.date(2002,1,2).isoformat())
-            ws.cell(row = row_counter , column = 1, value = row[4])
+        if "TX" in row[1] and "MTX" not in row[1] and "201607" in row[2]:
+            ws.cell(row = row_counter , column = 1, value = datetime.date(int(row[0][0:4]),int(row[0][4:6]),int(row[0][6:8])).isoformat())
+            ws.cell(row = row_counter , column = 2, value = datetime.time(int(row[3][0:2]),int(row[3][2:4]),int(row[3][4:6])))
+            ws.cell(row = row_counter , column = 3, value = row[4])
             row_counter = row_counter + 1
     except:
         pass
